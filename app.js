73206b1b8ae14b6c4201a745f9d2afff546067c9
amir200887 <amirhos20081387@gmail.com>
2025-08-22 01:10:07 +0330

    digitalclock

---
+++ app.js
@@ -0,0 +1,27 @@
+const hoursElem = document.querySelector(".hours");
+const minutesElem = document.querySelector(".minutes");
+const secondsElem = document.querySelector(".seconds");
+
+setInterval(() => {
+  const date = new Date();
+
+  let hour = date.getHours();
+  let minute = date.getMinutes();
+  let second = date.getSeconds();
+
+  if (hour < 10) {
+    hour = "0" + hour;
+  }
+
+  if (minute < 10) {
+    minute = "0" + minute;
+  }
+
+  if (second < 10) {
+    second = "0" + second;
+  }
+
+  hoursElem.innerHTML = hour;
+  minutesElem.innerHTML = minute;
+  secondsElem.innerHTML = second;
+},0);
